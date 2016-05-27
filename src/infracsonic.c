c9dd7c5fdd4cc4005ae1cc2908505eabcb5bbe71
Shawn <hongshan1008@163.com>
2016-05-27 22:43:43 +0800

    完善了超声波程序
    完善了超声波测距程序

--- src/infracsonic.c
+++ src/infracsonic.c
@@ -9,29 +9,54 @@ void InitTimer1()
 	TR1 = 0;
 }
 
-double distance()
-{	
-	double distmp = 0;	
-	uint tmp = 0,overflow = 0;//计算溢出次数
-	TL1 = 0x00;		//设置定时初值
-	TH1 = 0x00;		//设置定时初值
-	TRIG=1;	
+//double distance()
+//{	
+//	double distmp = 0;	
+//	uint tmp = 0,overflow = 0;//计算溢出次数
+//	TL1 = 0x00;		//设置定时初值
+//	TH1 = 0x00;		//设置定时初值
+//	TRIG=1;	
+//	Delay10us();
+//	Delay10us();
+//	TRIG=0;
+//	while(ECHO==0);
+//	TR1=1;		
+//	while(ECHO==1)
+//	{
+//		tmp = TH1;
+//		while(TH1 == tmp );
+//			if(tmp > TH1)
+//		{
+//			overflow++;
+//		}
+//	}	
+//	TR1=0;
+//	distmp= (overflow+(TH1*256+TL1)/(double)65535) * consume* 340.0 /2 * 100; 
+//	//distmp = overflow+(TH1*256+TL1);
+//	return distmp;
+//}
+
+double distance()	
+{
+
+ double distemp=0;
+ 
+
+   TRIG = 1;
+   Delay10us();
 	Delay10us();
-	Delay10us();
-	TRIG=0;
-	while(ECHO==0);
-	TR1=1;		
-	while(ECHO==1)
-	{
-		tmp = TH1;
-		while(TH1 == tmp );
-			if(tmp > TH1)
-		{
-			overflow++;
-		}
-	}	
-	TR1=0;
-	distmp= (overflow+(TH1*256+TL1)/(double)65535) * consume* 340.0 /2 * 100; 
-	//distmp = overflow+(TH1*256+TL1);
-	return distmp;
+   TRIG = 0;
+   while(ECHO==0);
+   TR1 = 1; //开定时器
+   
+  while(ECHO==1);
+    TR1=0;
+	
+
+	distemp= ((TH1*256+TL1)*1.08) *  340.0 /2 /10000; 
+//	printf("TL0 IS %bd",TL1);
+//	printf("TH0 IS %bd",TH1);
+	TH1 = 0;
+	TL1 = 0;
+	return distemp;
 }
